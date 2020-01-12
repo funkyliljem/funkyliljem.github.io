7cd90506a7b4b11207331a1f402e05a3eb89a766
funkyliljem <vic19010@byui.edu>
2020-01-11 14:16:10 -1000

    Change date 3

--- js/date.js
+++ js/date.js
@@ -39,6 +39,4 @@ try{
         year: "numeric",
     };
     document.getElementById("currentdate2").textContent = new Date().toLocaleDateString("en-US", options);
-} catch (e) {
-    alert("Error with code or your browser does not support Locale");  
 }
