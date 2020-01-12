bd0c9c8f482b6e2bb35de896711e6bf91ad90fb2
funkyliljem <vic19010@byui.edu>
2020-01-11 14:57:55 -1000

    year

--- js/date.js
+++ js/date.js
@@ -26,7 +26,7 @@ let months = [
 let d = new Date();
 let dayName = daynames[d.getDay()];
 let monthName = months[d.getMonth()];
-let fulldate = dayName + " , " + monthName + " " + d.getDate() + " , " + d.getFullYear();
+let fulldate = d.getFullYear();
 
 document.getElementById("currentdate").textContent = fulldate; 
 
