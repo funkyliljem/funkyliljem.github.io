601eb35fc01b77125fa31fbe49ef5f47c24994a3
funkyliljem <vic19010@byui.edu>
2020-02-05 21:39:20 -1000

    lesson 2 changes

---
+++ lesson_2/date.js
@@ -0,0 +1,31 @@
+let daynames = [
+    "Monday", 
+    "Tuesday", 
+    "Wednesday", 
+    "Thursday", 
+    "Friday", 
+    "Saturday", 
+    "Sunday"
+];
+
+let months = [
+    "January", 
+    "February", 
+    "March", 
+    "April", 
+    "May", 
+    "June", 
+    "July", 
+    "August", 
+    "September", 
+    "October", 
+    "November", 
+    "December"
+];
+
+let d = new Date();
+let dayName = daynames[d.getDay()];
+let monthName = months[d.getMonth()];
+let fulldate = d.getFullYear();
+
+document.getElementById("currentdate").textContent = fulldate; 
