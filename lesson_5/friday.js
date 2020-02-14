62f217c3c14bdcbae22bf53f50069fd632305c5f
funkyliljem <vic19010@byui.edu>
2020-02-13 22:05:21 -1000

    updates to js

--- lesson_5/friday.js
+++ lesson_5/friday.js
@@ -1,19 +1,8 @@
  
-let daynames = [
-  "Monday", 
-  "Tuesday", 
-  "Wednesday", 
-  "Thursday", 
-  "Friday", 
-  "Saturday", 
-  "Sunday"
-];
-
-let d = new Date();
-let dayName = daynames[d.getDay()];
-
-    if (daynames === "Friday") {
-      x.style.display = "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion";
-    }
-  document.getElementById("x.style.display");
-  
+function newFunction() {
+  var d = new Date();
+ if (d.getDay == 5) { 
+   document.getElementById("friday").innerHTML = "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion";
+  }
+}
+newFunction();
