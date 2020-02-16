21191bdb02e1ad2f4ea91c4ec30620a21a2d6c5b
funkyliljem <vic19010@byui.edu>
2020-02-15 15:34:09 -1000

    windchill3

--- lesson_6/windchill.js
+++ lesson_6/windchill.js
@@ -1,13 +1,15 @@
+
 const temp = parseFloat(document.getElementById('temp').textContent);
 const speed = parseFloat(document.getElementById('speed').textContent);
 
-const windChill = math.round(35.74+0.6215*temp-(35.75*(speed**0.16))+0.4275*(temp*(speed**0.16));
+const windChill = math.round(35.74+0.6215*temp-(35.75*(speed**0.16))+0.4275*(temp*(speed**0.16)));
 
-if (temp <= 50 && speed > 3) 
+if (temp <= 50 && speed > 3) {
     document.getElementById('windchill').innerHTML =`${windChill}`;
+}
 else {
     message = "N/A";
     }
-    document.getElementById('windchill').innerHTML =`${windChill}`;
+    document.getElementById('windchill').innerHTML = message;
 
 
