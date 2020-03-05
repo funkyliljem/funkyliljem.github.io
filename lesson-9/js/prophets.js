90293e0c550eee0eaa80ea00b2b4a46f22f2684c
funkyliljem <vic19010@byui.edu>
2020-03-05 01:52:12 -1000

    js working

--- lesson-9/js/prophets.js
+++ lesson-9/js/prophets.js
@@ -6,23 +6,30 @@ fetch(requestURL)
     return response.json();
   })
   .then(function (jsonObject) {
-    console.table(jsonObject) 
-    const prophets = jsonObject['prophets']
+    //console.table(jsonObject); 
+    const prophets = jsonObject['prophets'];
 
     for (let i = 0; i < prophets.length; i++ ) {
   
 let card = document.createElement('section');
 let h2 = document.createElement('h2');
-let image= document.createElement('image');
+let birthdate = document.createElement('p');
+let birthplace = document.createElement('p');
+let image = document.createElement('img');
 
-h2.textContent = prophets[i].name + ' ' + prophets[i].lastname + ' '
-+ prophets[i].birthdate + ' ' + prophets[i].birthplace;
+h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+birthdate.textContent = "Date of Birth:" + prophets[i].birthdate;
+birthplace.textContent = "Birth Place:" + prophets[i].birthplace;
+
 image.setAttribute('src', prophets[i].imageurl);
+image.setAttribute('alt', prophets[i].name + ' ' + prophets[i].lastname);
 
 card.appendChild(h2);
+card.appendChild(birthdate);
+card.appendChild(birthplace);
 card.appendChild(image);
 
 document.querySelector('div.cards').appendChild(card);
 
-    };
+    }
   });
