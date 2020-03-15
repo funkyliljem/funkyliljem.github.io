fde320835c85794fb5c4051556cf770b3b29a411
funkyliljem <vic19010@byui.edu>
2020-03-14 20:11:22 -1000

    weather summary

--- lesson-10/js/weatherapi2.js
+++ lesson-10/js/weatherapi2.js
@@ -14,6 +14,19 @@ fetch(apiURL)
     speed.textContent = jsObject.wind.speed;
 
 
+    const temp = parseFloat(document.getElementById('current-temp').textContent);
+    const speed = parseFloat(document.getElementById('speed').textContent);
+    
+    const windChill = Math.round(35.74+0.6215*temp-(35.75*(speed**0.16))+0.4275*(temp*(speed**0.16)));
+    
+    if (temp <= 50 && speed > 3) {
+        document.getElementById('windchill').innerHTML =`${windChill}`;
+    }
+    else {
+        message = "N/A";
+        }
+        document.getElementById('windchill').innerHTML = message;
+
 /*Five Day Forecast*/
 const apiFive = 'https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&APPID=4d740699e37a38e6fd92554001fa0cb3';
 
