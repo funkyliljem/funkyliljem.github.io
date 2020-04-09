650cc70157172b5ae5ec694485ef1f99bc1c9b2d
funkyliljem <vic19010@byui.edu>
2020-04-09 09:36:47 -1000

    html chck

--- lesson-14/js/windchill.js
+++ lesson-14/js/windchill.js
@@ -1,11 +1,11 @@
 const apiURL3 = 'https://api.openweathermap.org/data/2.5/weather?zip=83549&units=imperial&APPID=4d740699e37a38e6fd92554001fa0cb3';
 
-const temp = parseFloat(document.getElementById('current-temp').textContent);
+const temp = parseFloat(document.getElementById('temp').textContent);
 const speed = parseFloat(document.getElementById('speed').textContent);
 
-const windChill = Math.round(35.74+0.6215*temp-(35.75*(speed**0.16))+0.4275*(temp*(speed**0.16)));
+const windChill = Math.round(35.74+0.6215*jsObect.main.temp-(35.75*(jsObect.main.speed**0.16))+0.4275*(jsObect.main.temp*(jsObect.main.speed**0.16)));
 
-if (temp <= 50 && speed > 3) {
+if (jsObect.main.temp <= 50 && jsObect.main.speed > 3) {
     document.getElementById('windchill').innerHTML = windChill;
 }
 else {
